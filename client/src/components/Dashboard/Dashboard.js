4858ee054cae43b3072ba859ba10b52ad9b2c020
Esteban Quintana <esteban.quintana@wizeline.com>
2020-03-14 13:16:04 -0600

    dashboard

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -3,13 +3,7 @@ import PropTypes from "prop-types";
 
 //Package imports
 import { 
-  Button,
-  Col, 
   Container,
-  Form,
-  FormControl,
-  Nav, 
-  Navbar,
   Row,
 } from 'react-bootstrap';
 //redux
@@ -17,11 +11,8 @@ import { connect } from "react-redux";
 import { loginUser } from "../../actions/authActions";
 import { logoutUser } from "../../actions/authActions";
 
-import ClientesForm from "../CLIENTES/ClientesForm/ClientesForm";
-import SecomForm from "../SECOM/SecomForm/SecomForm";
-import SecomReport from "../SECOM/SecomReport/SecomReport";
-import ClientsReport from "../CLIENTES/ClientsReport/ClientsReport";
-import ReferidosReport from "../ReferidosReport/ReferidosReport";
+import Sidebar from "../Sidebar/Sidebar";
+import Navbar from "../Navbar/Navbar";
 
 import "./Dashboard.css";
 
@@ -47,107 +38,38 @@ class Dashboard extends Component {
     this.props.history.push("/admin");
   }
 
-  selectForm = () => {
-    let selected;
-
-    switch(this.state.selectedForm){
-      case "clientes":
-        selected = <ClientesForm></ClientesForm>;
-        break;
-      case "secom":
-        selected = <SecomForm></SecomForm>;
-        break;
-      case "reportesS":
-        selected = <SecomReport></SecomReport>;
-        break;
-      case "reportesC":
-        selected = <ClientsReport></ClientsReport>;
-        break;
-      case "reportesR":
-        selected = <ReferidosReport></ReferidosReport>;
-        break;
-      default:
-        selected = <></>
-    }
-
-    return selected;
-  } 
-
   render() {
     const user = this.props.auth.user;
 
-    let selected = this.selectForm();
-
-    let content = null;
-
-    if(this.state.selectedForm === "reportesC"){
-      content = (<Container fluid>
-                  <Row className="mt-4">
-                      {selected}
-                  </Row>
-                  </Container>);      
-    } else {
-      content = (<Container>
-        <Row className="mt-4">
-            {selected}
-        </Row>
-        </Container>);      
-    }
-
     return (
-      <Col sm={12}>
-        <Navbar bg="light">
-          <Navbar.Brand href="#home">
-            <img
-              src="static/img/texto.png"
-              height="50"
-              alt="logo"
-              className="d-inline-block align-top"
-            />
-          </Navbar.Brand>
-        </Navbar>
-        <Navbar collapseOnSelect expand="lg" className="bg-blue" variant="dark">
-          <Navbar.Brand href="#home">{user.name} {user.last_name}</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link onClick={this.onLogoutClick}>Cerrar sesion</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-          { user.role === "admin" &&
-          <Form inline>
-            <Button onClick={this.adminPage}>Ir a panel de admin</Button>
-          </Form>
-          }
-        </Navbar>
-        <Row className="mt-4 justify-content-md-center">
-            <Col md="5">
-              <table className="buttons">
-                <tr>
-                  <td>
-                    <Button type="button" onClick={(e) => this.chooseForm(e, "clientes")}>CLIENTES</Button>
-                  </td>
-                  <td>
-                    <Button type="button" onClick={(e) => this.chooseForm(e, "secom")}>SECOM</Button>
-                  </td>
-                  <td>
-                    <Button type="button" onClick={(e) => this.chooseForm(e, "reportesC")}>Reportes CLIENTES</Button>
-                  </td>
-                  <td>
-                    <Button type="button" onClick={(e) => this.chooseForm(e, "reportesS")}>Reportes SECOM</Button>
-                  </td>
-                  <td>
-                    <Button type="button" onClick={(e) => this.chooseForm(e, "reportesR")}>Referidos</Button>
-                  </td>
-                </tr>
-              </table>
-            </Col>
-          </Row>
+      <div class="wrapper">
 
-          <div>
-            {content}
-          </div>
-      </Col>
+        <Sidebar></Sidebar>
+
+        <div id="content">
+
+            <Navbar></Navbar>
+
+            <h2>Collapsible Sidebar Using Bootstrap 4</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+            <div class="line"></div>
+
+            <h2>Lorem Ipsum Dolor</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+            <div class="line"></div>
+
+            <h2>Lorem Ipsum Dolor</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+            <div class="line"></div>
+
+            <h3>Lorem Ipsum Dolor</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+    </div>
     );
   }
 }
--- client/src/components/UI/Tabs/Tabs.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Tabs from './Tabs';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<NavBar />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
