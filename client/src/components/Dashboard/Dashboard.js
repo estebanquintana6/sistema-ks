c2e05caa248d7f5cf358069e5d4da190780c304b
Esteban Quintana <esteban.quintana@wizeline.com>
2020-03-15 10:10:57 -0600

    new dashboard

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,11 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-//Package imports
-import { 
-  Container,
-  Row,
-} from 'react-bootstrap';
 //redux
 import { connect } from "react-redux";
 import { loginUser } from "../../actions/authActions";
@@ -25,40 +20,12 @@ class Dashboard extends Component {
     };
   }
 
-  chooseForm = (e, key) => {
-    this.setState({selectedForm: key})
-  }
-
-  adminPage = (e) => {
-    this.props.history.push("/admin");
-  }
-
   render() {
-    const user = this.props.auth.user;
-
     return (
-      <div class="wrapper">
+      <div className="wrapper">
         <Sidebar></Sidebar>
         <div id="content">
             <Navbar></Navbar>
-            <h2>Collapsible Sidebar Using Bootstrap 4</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-            <div class="line"></div>
-
-            <h2>Lorem Ipsum Dolor</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-            <div class="line"></div>
-
-            <h2>Lorem Ipsum Dolor</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-            <div class="line"></div>
-
-            <h3>Lorem Ipsum Dolor</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
     </div>
     );
--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -1,62 +1,82 @@
 import React, { Component } from "react"
+import PropTypes from "prop-types";
+
+import { connect } from "react-redux";
+
 
 class Sidebar extends Component {
-  render() {
-    return (
-        <nav id="sidebar">
-            <div className="sidebar-header">
-                <h3>KS Seguros</h3>
-            </div>
-            <ul className="list-unstyled components">
-                <li className="active">
-                    <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">Clientes</a>
-                    <ul className="collapse list-unstyled" id="homeSubmenu">
-                        <li>
-                            <a href="#">Registro de clientes</a>
-                        </li>
-                        <li>
-                            <a href="#">Panel de clientes</a>
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">Auto</a>
-                    <ul className="collapse list-unstyled" id="pageSubmenu">
-                        <li>
-                            <a href="#">Registro de seguro</a>
-                        </li>
-                        <li>
-                            <a href="#">Panel de seguro</a>
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="#autoSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">Gastos médicos</a>
-                    <ul className="collapse list-unstyled" id="autoSubmenu">
-                        <li>
-                            <a href="#">Registro de seguro</a>
-                        </li>
-                        <li>
-                            <a href="#">Panel de seguro</a>
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="#">Contact</a>
-                </li>
-            </ul>
+    render() {
+        const user = this.props.auth.user;
 
-            <ul className="list-unstyled CTAs">
-                <li>
-                    <a href="https://bootstrapious.com/tutorial/files/sidebar.zip" className="download">Perfil</a>
-                </li>
-                <li>
-                    <a href="https://bootstrapious.com/p/bootstrap-sidebar" className="article">Panel de administrador</a>
-                </li>
-            </ul>
-        </nav>
-    );
-  }
-}
+        return (
+            <nav id="sidebar">
+                <div className="sidebar-header">
+                    <h3>KS Seguros</h3>
+                </div>
+                <ul className="list-unstyled components">
+                    <li>
+                        <a href="#clienteSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">Clientes</a>
+                        <ul className="collapse list-unstyled" id="clienteSubmenu">
+                            <li>
+                                <a href="#">Registro de clientes</a>
+                            </li>
+                            <li>
+                                <a href="#">Panel de clientes</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#autoSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">Auto</a>
+                        <ul className="collapse list-unstyled" id="autoSubmenu">
+                            <li>
+                                <a href="#">Registro de seguro</a>
+                            </li>
+                            <li>
+                                <a href="#">Panel de seguro</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#medicoSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">Gastos médicos</a>
+                        <ul className="collapse list-unstyled" id="medicoSubmenu">
+                            <li>
+                                <a href="#">Registro de seguro</a>
+                            </li>
+                            <li>
+                                <a href="#">Panel de seguro</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
 
-export default Sidebar;
+                <ul className="list-unstyled CTAs">
+                    <li>
+                        <a href="#" className="download">Perfil</a>
+                    </li>
+                    { user.role === "admin" &&
+                    <li>
+                        <a href="/admin" className="article">Panel de administrador</a>
+                    </li>
+                    }
+                </ul>
+            </nav>
+        );
+    }
+    }
+
+Sidebar.propTypes = {
+    auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+    auth: state.auth,
+    errors: state.errors
+});
+  
+export default connect(
+    mapStateToProps,
+    {}
+)(Sidebar);
