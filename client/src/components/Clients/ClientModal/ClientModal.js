9e5caff48fc9eb49b73a819f0892a4d846ccc430
Esteban Quintana <esteban.quintana@wizeline.com>
2020-04-04 15:06:21 -0600

    Agregar contactos a clientes

--- client/src/components/Clients/ClientModal/ClientModal.js
+++ client/src/components/Clients/ClientModal/ClientModal.js
@@ -8,16 +8,27 @@ import {
 } from 'react-bootstrap';
 import ClientsForm from "../ClientsForm/ClientsForm";
 
+import "./ClientModal.css";
+import swal from '@sweetalert/with-react';
+
 class ClientModal extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      edit: null
-    }
   }
 
   editUser = () => {
-    this.setState({ edit: true })
+    const { client } = this.props;
+    swal({
+      title: `${client.name} ${client.last_name}`,
+      text: "Edita los campos y presiona guardar para modificar la información del cliente",
+      content: <ClientsForm 
+                  client={client} 
+                  edit={true} 
+                  updateClient={this.props.updateClient}>
+                </ClientsForm>,
+      className: "width-800pt" ,
+      buttons: false
+    });
   }
 
   render() {
@@ -25,7 +36,6 @@ class ClientModal extends Component {
     return (
       < Container >
         {
-          this.state.edit ? <ClientsForm client={client} edit={this.state.edit} updateClient={this.props.updateClient} ></ClientsForm> :
             <React.Fragment>
               <Row className="mt-4">
                 <Col>
@@ -35,10 +45,10 @@ class ClientModal extends Component {
               <br></br>
               <Row>
                 <Col>
-                  <Button variant="info" onClick={this.editUser}>Editar</Button>
+                  <Button className="panel-btn" variant="info" onClick={this.editUser}>EDITAR</Button>
                 </Col>
                 <Col>
-                  <Button variant="danger" onClick={this.props.deleteClient.bind(this, client._id, client.name)}>ELIMINAR</Button>
+                  <Button className="panel-btn" variant="danger" onClick={this.props.deleteClient.bind(this, client._id, client.name)}>ELIMINAR</Button>
                 </Col>
               </Row>
             </React.Fragment>
