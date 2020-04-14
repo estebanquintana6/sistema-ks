6cb46a4ecbe6ed834994468aa1253601c8b5c0da
MartinMolinero <malej.mol@gmail.com>
2020-04-13 19:58:03 -0500

    file uploader

---
+++ client/src/components/SimpleReactFileUpload/SimpleReactFileUpload.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import axios, { post } from 'axios';
+
+class SimpleReactFileUpload extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state ={
+      file:null
+    }
+    this.onFormSubmit = this.onFormSubmit.bind(this)
+    this.onChange = this.onChange.bind(this)
+    this.fileUpload = this.fileUpload.bind(this)
+  }
+  onFormSubmit(e){
+    e.preventDefault() // Stop form submit
+    this.fileUpload(this.state.file).then((response)=>{
+      console.log(response.data);
+    })
+  }
+  onChange(e) {
+    this.setState({file:e.target.files[0]})
+  }
+  fileUpload(file){
+    const url = `${'/'}`;
+    const formData = new FormData();
+    axios.post(post, {url, formData}).then(() => {})
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.onFormSubmit}>
+        <h1>File Upload</h1>
+        <input type="file" onChange={this.onChange} />
+        <button type="submit">Upload</button>
+      </form>
+   )
+  }
+}
+
+
+
+export default SimpleReactFileUpload
