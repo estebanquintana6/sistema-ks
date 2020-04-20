218fadc2d665152550821cd61cbe9222d79189a2
Esteban Quintana <esteban.quintana@wizeline.com>
2020-04-20 18:42:12 -0500

    pendientes fixes

--- client/src/components/Tasks/TaskForm/TaskForm.js
+++ client/src/components/Tasks/TaskForm/TaskForm.js
@@ -13,12 +13,14 @@ import { cloneDeep } from 'lodash'
 class TaskForm extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      assignee: []
-    };
+    if(!this.props.edit){
+      this.state = {
+        assignee: [""]
+      };
+    }
   }
 
-  componentDidMount() {
+  componentWillMount() {
     if (!this.props.edit) return;
     // prepare the Task data to be rendered in every field
     this.prepareTaskForForm();
@@ -26,27 +28,35 @@ class TaskForm extends Component {
 
   prepareTaskForForm = () => {
     const auxObj = cloneDeep(this.props.task)
-    auxObj['edit'] = this.props['edit']
-    this.setState(auxObj)
+    auxObj['edit'] = this.props['edit'];
+    this.setState(auxObj);
   }
 
   onChange = e => {
     this.setState({ [e.target.id]: e.target.value });
   };
 
-  addAsignee = e => {
+  addAsignee = (index, e) => {
     let { assignee } = this.state;
-    assignee.push(e.target.value);
+    assignee[index] = e.target.value;
     this.setState({assignee});
-    document.getElementById(e.target.id).value = "";
+  }
+
+  createAssignee = () => {
+    const assignee = this.state.assignee;
+    assignee.push("");
+    this.setState({ assignee });
   }
 
   getUserName = (id) => {
     let user = this.props.users.filter((user) => {
       return user._id === id
     });
-    console.log(user);
     if(user.length > 0) return user[0].name;
+  }
+
+  getActive = (assignee, user) => {
+    return assignee === user._id;
   }
 
   onSubmit = e => {
@@ -62,66 +72,63 @@ class TaskForm extends Component {
   formatDate = (date) => moment(date).format('YYYY-MM-DD')
 
   render() {
+    console.log(this.state.assignee);
     return (
       <Form id="taskForm" onSubmit={this.onSubmit}>
         <Row>
           <h5 className="swal-title form-title align-left">GENERALES</h5>
         </Row>
         <Row>
-          <Col md={12}>
+          <Col>
             <Form.Row>
               <Form.Group as={Col} controlId="title">
-                <Form.Label>Mensaje</Form.Label>
+                <Form.Label>Asunto</Form.Label>
                 <Form.Control required onChange={this.onChange} value={this.state.title} />
               </Form.Group>
             </Form.Row>
           </Col>
-        </Row>
-        <Row>
-        <Col>
+          <Col>
               <Form.Group controlId="status">
                 <Form.Label>Estatus</Form.Label>
                 <Form.Control required as="select" onChange={this.onChange} value={this.state.status}>
                   <option></option>
-                  <option value="Nuevo">Nuevo</option>
-                  <option value="En progreso">En progreso</option>
-                  <option value="Bloqueado">Bloqueado</option>
+                  <option value="Pendiente">Pendiente</option>
                   <option value="Terminado">Terminado</option>
                 </Form.Control>
               </Form.Group>
           </Col>
-          <Col>
-          <Form.Group controlId="due_date">
-            <Form.Label>Fecha de entrega</Form.Label>
-            <Form.Control type="date" onChange={this.onChange} value={this.formatDate(this.state.due_date)}>
-            </Form.Control>
-          </Form.Group>
-          </Col>
         </Row>
+
         <Row>
           <h5 className="swal-title form-title align-left">ASGINADOS</h5>
         </Row>
-        <Row>
+          <Row className="pt-1 pb-2">
+                    <Col md="12">
+                      <Button variant="info" onClick={this.createAssignee}>AGREGAR</Button>
+                    </Col>
+                  </Row>
+          <Row>
           <Col>
-              <Form.Group controlId="assignee">
-                <Form.Label>Asignado a:</Form.Label>
-                <Form.Control as="select" onChange={this.addAsignee}>
-                  <option></option>
-                  {this.props.users.filter((user) => {
-                                      return !this.state.assignee.includes(user._id)
-                                    })
-                                    .map((user, index) => 
-                                        <option value={user._id} key={index}>{user.name}</option>
-                                    )}
-                </Form.Control>
-              </Form.Group>
+              {this.state.assignee.map((assignee, index) => {
+              return (
+                <Form.Group controlId="assignee">
+                  <Form.Label>Asignado a:</Form.Label>
+                  <Form.Control as="select" onChange={this.addAsignee.bind(this, index)} value={this.state.assignee[index]}>
+                    <option></option>
+                    {this.props.users.map((user, index) => 
+                                          <option 
+                                                  selected={this.getActive.bind(assignee, user)} 
+                                                  value={user._id} 
+                                                  key={index}>{user.name}
+                                          </option>
+                                      )
+                    }
+                  </Form.Control>
+                </Form.Group>
+              );
+              })}
           </Col>
           <Col>
-          <ul class="list-group">
-            {this.state.assignee.map((user) => {
-              return <li class="list-group-item">{this.getUserName(user)}</li>
-            })}
-          </ul>
           </Col>
         </Row>
         <Button variant="primary" type="submit">Guardar</Button>
