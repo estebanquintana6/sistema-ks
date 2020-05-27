4b89024e83c716814ea44b3aadb2e2ce2fb956ff
MartinMolinero <malej.mol@gmail.com>
2020-05-26 22:07:13 -0500

    debug

--- client/src/components/ExportCSV/ExportCSV.js
+++ client/src/components/ExportCSV/ExportCSV.js
@@ -5,6 +5,7 @@ import * as XLSX from 'xlsx';
 import moment from 'moment'
 
 function renameLabel(json, oldLabel, newLabel) {
+    if (!newLabel) console.log('NULL FOR' oldLabel)
     json["" + newLabel] = json["" + oldLabel]
     const tmp = json["" + oldLabel]
     const tmpDate = moment(tmp, moment.ISO_8601)
