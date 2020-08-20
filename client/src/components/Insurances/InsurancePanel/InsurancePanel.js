075723c084e300552c9f173d54d6fde95d4a7c4e
Esteban Quintana <esteban.quintana@wizeline.com>
2020-08-20 11:17:55 -0500

    danos excel

--- client/src/components/Insurances/InsurancePanel/InsurancePanel.js
+++ client/src/components/Insurances/InsurancePanel/InsurancePanel.js
@@ -54,7 +54,7 @@ class InsurancePanel extends Component {
       data: [],
       clients: [],
       companies: [],
-      excludedFields: this.props.variant === 'GMM' ?
+      excludedFields: this.props.variant === 'GM' ?
         ['__v', '_id', 'files', 'active_status', 'endorsements', 'comments', 'status', 'created_at', 'tolerance', 'state', 'city', 'postal_code', 'gender', 'contacts', 'begin_date', 'invoices', 'cancelation_note', 'car_model', 'languages', 'damage_product', 'bounty', 'currency'] :
         ['__v', '_id', 'files', 'active_status', 'endorsements', 'comments', 'status', 'created_at', 'tolerance', 'state', 'city', 'postal_code', 'gender', 'contacts', 'begin_date', 'invoices', 'cancelation_note', 'car_model', 'languages', 'damage_product']
     };
@@ -115,14 +115,14 @@ class InsurancePanel extends Component {
       }
     }
 
-    if (this.props.variant !== 'GMM') {
+    if (this.props.variant !== 'GM') {
       resObj = {
         ...resObj,
         bounty: "Prima",
         currency: "Moneda"
       }
     }
-    
+
     return resObj
   }
 
